72900635f24a4fad5ac9a2caecbf362161fddf5e
Aakriti Agrahari <agrahariaakriti1@gmail.com>
2025-12-01 20:01:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("<h1>Welcom to twitter</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
